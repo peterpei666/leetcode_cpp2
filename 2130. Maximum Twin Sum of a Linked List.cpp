260171960cb9c122738943337372bc2c7d1e2824
Peter_Pei <mmiaomiao140@gmail.com>
2026-06-14 22:27:19 +0800

    Add files via upload

---
+++ 2130. Maximum Twin Sum of a Linked List.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    int pairSum(ListNode* head)
+    {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+        while (fast && fast->next)
+        {
+            fast = fast->next->next;
+            swap(slow->next, prev);
+            swap(slow, prev);
+        }
+        int ans = 0;
+        while (slow)
+        {
+            ans = max(ans, prev->val + slow->val);
+            prev = prev->next;
+            slow = slow->next;
+        }
+        return ans;
+    }
+};
